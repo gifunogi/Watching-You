7231a8137bb9c44d324686835a765f4bd052083b
gifunogi <gifunogi@gmail.com>
2016-05-02 03:09:46 +0900

    削除されたツイートを保存するように，README.mdの詳細化．

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # Watching-You
 消された過去を掘り返す。
+
+
+自分のタイムライン上でTweetが消されたとき，それを通知してデータベースファイルに保存するプログラムです．pythonのTwitter APIモジュールである[tweepy](https://github.com/tweepy/tweepy)を使用しています．
+
+```
+CK = ""
+CS = ""
+AT = ""
+AS = ""
+```
+に，それぞれ[Twitter Developers](https://dev.twitter.com/)で取得したConsumer Key，Consumer Key Secret，Access Token，Access Token Secretを入力し，`python watchingYou.py`のコマンドからプログラムの起動が可能です．
+プログラムの起動後から，自身のタイムラインのツイート情報を`tweet.db`ファイルに保存します．もしも，タイムライン上でツイートの削除が行えわれ，かつ`tweet.db`ファイル内に該当データがあった場合，
+`--****(@****)さんが以下のツイートをツイ消ししました．--`
+と表示し，そのデータを`deleted.db`ファイルにコピーします．
--- watchingYou.py
+++ watchingYou.py
@@ -5,10 +5,10 @@ import sqlite3
 from tweepy.streaming import StreamListener, Stream
 from tweepy.auth import OAuthHandler
 from tweepy.api import API
-from datetime import timedelta
 
 # データベースのパス
-DB_PATH = "tweet.db"
+TWEET_PATH = "tweet.db"
+DELETED_PATH = "deleted.db"
 
 # 認証情報
 CK = ""
@@ -21,18 +21,27 @@ def get_oauth():
 	auth.set_access_token(AT, AS)
 	return auth
 
-class CustomStreamListener(StreamListener):
+class MyStreamListener(StreamListener):
 	def on_data(self, data):
 		stream = json.loads(data)
 		if 'delete' in stream:
 			user_id = stream['delete']['status']['user_id']
 			tweet_id = stream['delete']['status']['id']
-			if not isinstance(con.execute("SELECT * FROM '{user_id}' where tweet_id = '{tweet_id}'".format(user_id=user_id, tweet_id=tweet_id)).fetchone(), type(None)):
-				name = api.get_user(user_id).name
-				screen = api.get_user(user_id).screen_name
-				tweet = con.execute("SELECT * FROM '{user_id}' where tweet_id = '{tweet_id}'".format(user_id=user_id, tweet_id=tweet_id)).fetchone()[1]
-				print(u"""--{name}(@{screen})さんが次のツイートをツイ消ししました。--""".format(name=name, screen=screen))
-				print(u"{tweet}\n".format(tweet=tweet))
+			screen = api.get_user(user_id).screen_name
+			cur = tweet_DB.execute("SELECT * FROM sqlite_master WHERE type='table' and name='{screen}'".format(screen=screen))
+			if not cur.fetchone() is None:
+				deleted = tweet_DB.execute("SELECT * FROM '{screen}' where tweet_id = '{tweet_id}'".format(screen=screen, tweet_id=tweet_id)).fetchone()
+				if not isinstance(deleted, type(None)):
+					name = api.get_user(user_id).name
+					tweet = deleted[1]
+					print(u"--{name}(@{screen})さんが以下のツイートをツイ消ししました．--".format(name=name, screen=screen))
+					print(u"{tweet}\n".format(tweet=tweet))
+					try: deleted_DB.execute("CREATE TABLE '{screen}'(tweet_id, tweet, created_at)".format(screen=screen))
+					except: pass
+					# try: deleted_DB.execute("INSERT INTO '{screen}'VALUES{deleted}".format(screen=screen, deleted=deleted))
+					# except: pass
+					deleted_DB.execute("INSERT INTO '{screen}'VALUES{deleted}".format(screen=screen, deleted=deleted))
+					deleted_DB.commit()
 			
 		elif 'text' in stream:
 			tweet = stream['text'].replace("'", '"')
@@ -44,18 +53,17 @@ class CustomStreamListener(StreamListener):
 			print(u"{tweet}".format(tweet=tweet))
 			print(u"{name}(@{screen}) {created_at}".format(name=name, screen=screen, created_at=created_at))
 			print(u"TweetID: {tweet_id}\n".format(tweet_id=tweet_id))
-			try: con.execute("CREATE TABLE '{user_id}'(tweet_id, tweet)".format(user_id=user_id))
+			try: tweet_DB.execute("CREATE TABLE '{screen}'(tweet_id, tweet, created_at)".format(screen=screen))
 			except: pass
-			try: con.execute("INSERT INTO '{user_id}'VALUES('{tweet_id}', '{tweet}')".format(user_id=user_id, tweet_id=tweet_id, tweet=tweet))
+			try: tweet_DB.execute("INSERT INTO '{screen}'VALUES('{tweet_id}', '{tweet}', '{created_at}')".format(screen=screen, tweet_id=tweet_id, tweet=tweet, created_at=created_at))
 			except: pass
-			con.commit()
+			tweet_DB.commit()
 
 if __name__ == '__main__':
 	auth = get_oauth()
 	api = API(auth_handler=auth, retry_count=1)
-	con = sqlite3.connect(DB_PATH)
-	stream = Stream(auth, CustomStreamListener(), secure=True)
+	tweet_DB = sqlite3.connect(TWEET_PATH)
+	deleted_DB = sqlite3.connect(DELETED_PATH)
+	stream = Stream(auth, MyStreamListener(), secure=True)
 	stream.userstream()
 	
-
-
