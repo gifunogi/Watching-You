6201ec0c99b387085855ceccbedba72dabd05a4a
HOUKAMPLIFIER\gifunogi <gifunogi@gmail.com>
2016-09-14 23:53:21 +0900

    文字に色を付けるように，再接続を試行するように，README.mdの文言の修正，.gitignoreの追加

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
 # Watching-You
-消された過去を掘り返す。
+お前を見ているぞ．
 
-
-自分のタイムライン上でTweetが消されたとき，それを通知してデータベースファイルに保存するプログラムです．pythonのTwitter APIモジュールである[tweepy](https://github.com/tweepy/tweepy)を使用しています．
+自分のタイムライン上でツイートが消されたとき，それを通知してデータベースファイルに保存するプログラムです．PythonのTwitter APIモジュールである[tweepy](https://github.com/tweepy/tweepy)を使用しています．
 
 ```
 CK = ""
@@ -10,7 +9,30 @@ CS = ""
 AT = ""
 AS = ""
 ```
-に，それぞれ[Twitter Developers](https://dev.twitter.com/)で取得したConsumer Key，Consumer Key Secret，Access Token，Access Token Secretを入力し，`python watchingYou.py`のコマンドからプログラムの起動が可能です．
-プログラムの起動後から，自身のタイムラインのツイート情報を`tweet.db`ファイルに保存します．もしも，タイムライン上でツイートの削除が行えわれ，かつ`tweet.db`ファイル内に該当データがあった場合，
-`--****(@****)さんが以下のツイートをツイ消ししました．--`
-と表示し，そのデータを`deleted.db`ファイルにコピーします．
+以上に，それぞれ[Twitter Developers](https://dev.twitter.com/)で取得したConsumer Key，Consumer Key Secret，Access Token，Access Token Secretを入力し，
+```
+python watchingYou.py
+```
+のコマンドからプログラムの起動が可能です．
+プログラムの起動後から，自身のタイムラインのツイート情報を`tweet.db`ファイルに保存します．また，ターミナル上ではタイムラインにツイートを取得した際に，
+```
+[ツイート文字列]
+****(@****) [Week] [Month] DD HH:MM:SS +0000 YYYY
+TweetID: ******************
+```
+と言った書式で出力を行います．
+
+もしも，タイムライン上でツイートの削除が行えわれ，かつ`tweet.db`ファイル内に該当データがあった場合，
+```
+--****(@****)さんが以下のツイートを消しました．--．--
+```
+と表示し，そのツイートデータを`deleted.db`ファイルにコピーします．
+
+UserStreamの接続にした場合は，
+```
+ConnectionError!
+Reconnect after 10 seconds.
+```
+と表示し，10秒後に再接続を行います．
+
+プログラムの終了には`Ctrl + c`，もしくはターミナルの終了から行って下さい．
--- watchingYou.py
+++ watchingYou.py
@@ -1,5 +1,9 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+
 import re
+import sys
+import time
 import json
 import sqlite3
 from tweepy.streaming import StreamListener, Stream
@@ -16,54 +20,67 @@ CS = ""
 AT = ""
 AS = ""
 
-def get_oauth():
-	auth = OAuthHandler(CK, CS)
-	auth.set_access_token(AT, AS)
-	return auth
-
 class MyStreamListener(StreamListener):
-	def on_data(self, data):
-		stream = json.loads(data)
-		if 'delete' in stream:
-			user_id = stream['delete']['status']['user_id']
-			tweet_id = stream['delete']['status']['id']
-			screen = api.get_user(user_id).screen_name
-			cur = tweet_DB.execute("SELECT * FROM sqlite_master WHERE type='table' and name='{screen}'".format(screen=screen))
-			if not cur.fetchone() is None:
-				deleted = tweet_DB.execute("SELECT * FROM '{screen}' where tweet_id = '{tweet_id}'".format(screen=screen, tweet_id=tweet_id)).fetchone()
-				if not isinstance(deleted, type(None)):
-					name = api.get_user(user_id).name
-					tweet = deleted[1]
-					print(u"--{name}(@{screen})さんが以下のツイートをツイ消ししました．--".format(name=name, screen=screen))
-					print(u"{tweet}\n".format(tweet=tweet))
-					try: deleted_DB.execute("CREATE TABLE '{screen}'(tweet_id, tweet, created_at)".format(screen=screen))
-					except: pass
-					# try: deleted_DB.execute("INSERT INTO '{screen}'VALUES{deleted}".format(screen=screen, deleted=deleted))
-					# except: pass
-					deleted_DB.execute("INSERT INTO '{screen}'VALUES{deleted}".format(screen=screen, deleted=deleted))
-					deleted_DB.commit()
-			
-		elif 'text' in stream:
-			tweet = stream['text'].replace("'", '"')
-			name = stream['user']['name']
-			screen = stream['user']['screen_name']
-			user_id = stream['user']['id']
-			created_at = stream['created_at']
-			tweet_id = stream['id']
-			print(u"{tweet}".format(tweet=tweet))
-			print(u"{name}(@{screen}) {created_at}".format(name=name, screen=screen, created_at=created_at))
-			print(u"TweetID: {tweet_id}\n".format(tweet_id=tweet_id))
-			try: tweet_DB.execute("CREATE TABLE '{screen}'(tweet_id, tweet, created_at)".format(screen=screen))
-			except: pass
-			try: tweet_DB.execute("INSERT INTO '{screen}'VALUES('{tweet_id}', '{tweet}', '{created_at}')".format(screen=screen, tweet_id=tweet_id, tweet=tweet, created_at=created_at))
-			except: pass
-			tweet_DB.commit()
+    def on_data(self, data):
+        stream = json.loads(data)
+        if 'delete' in stream:
+            user_id = stream['delete']['status']['user_id']
+            tweet_id = stream['delete']['status']['id']
+            screen = api.get_user(user_id).screen_name
+            cur = tweet_DB.execute("SELECT * FROM sqlite_master WHERE type='table' and name='{screen}'".format(screen=screen))
+            if not cur.fetchone() is None:
+                deleted = tweet_DB.execute("SELECT * FROM '{screen}' where tweet_id = '{tweet_id}'".format(screen=screen, tweet_id=tweet_id)).fetchone()
+                if not isinstance(deleted, type(None)):
+                    name = api.get_user(user_id).name
+                    tweet = deleted[1]
+                    print("--{name}(@{screen})さんが以下のツイートを消しました．--".format(name=name, screen=screen))
+                    print("\033[36m" + "{tweet}\n".format(tweet=tweet) + "\033[39m")
+                    try:
+                        deleted_DB.execute("CREATE TABLE '{screen}'(tweet_id, tweet, created_at)".format(screen=screen))
+                    except:
+                        pass
+                    deleted_DB.execute("INSERT INTO '{screen}'VALUES{deleted}".format(screen=screen, deleted=deleted))
+                    deleted_DB.commit()
+            
+        elif 'text' in stream:
+            tweet = stream['text'].replace("'", '"')
+            name = stream['user']['name']
+            screen = stream['user']['screen_name']
+            user_id = stream['user']['id']
+            created_at = stream['created_at']
+            tweet_id = stream['id']
+            print("\033[36m" + "{tweet}".format(tweet=tweet) + "\033[39m")
+            print("{name}(@{screen}) {created_at}".format(name=name, screen=screen, created_at=created_at))
+            print("TweetID: {tweet_id}\n".format(tweet_id=tweet_id))
+            try:
+                tweet_DB.execute("CREATE TABLE '{screen}'(tweet_id, tweet, created_at)".format(screen=screen))
+            except:
+                pass
+            try:
+                tweet_DB.execute("INSERT INTO '{screen}'VALUES('{tweet_id}', '{tweet}', '{created_at}')".format(screen=screen, tweet_id=tweet_id, tweet=tweet, created_at=created_at))
+            except:
+                pass
+            tweet_DB.commit()
+
+def get_oauth():
+    auth = OAuthHandler(CK, CS)
+    auth.set_access_token(AT, AS)
+    return auth
 
 if __name__ == '__main__':
-	auth = get_oauth()
-	api = API(auth_handler=auth, retry_count=1)
-	tweet_DB = sqlite3.connect(TWEET_PATH)
-	deleted_DB = sqlite3.connect(DELETED_PATH)
-	stream = Stream(auth, MyStreamListener(), secure=True)
-	stream.userstream()
-	
+    auth = get_oauth()
+    api = API(auth_handler=auth, retry_count=1)
+    tweet_DB = sqlite3.connect(TWEET_PATH)
+    deleted_DB = sqlite3.connect(DELETED_PATH)
+    stream = Stream(auth, MyStreamListener(), secure=True)
+    while True:
+        try:
+            stream.userstream()
+        except:
+            print("\033[31m" + "ConnectionError!")
+            print("Reconnect after 10 seconds." + "\033[39m")
+            for i in range(11):
+                print(".", end="")
+                sys.stdout.flush()
+                time.sleep(1)
+            print()
